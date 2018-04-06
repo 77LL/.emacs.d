ffd5d865f0cc90881894d553e19b3646bf005e24
Naoki Wakisaka <naoki.wakisaka@ams.eng.osaka-u.ac.jp>
2018-04-06 09:55:07 +0900

    Fix yasnippet

--- init/yasnippet.el
+++ init/yasnippet.el
@@ -10,9 +10,18 @@
 (yas-global-mode 1)
 
 ;; key bindings
-;; (custom-set-variables '(yas-trigger-key "TAB"))
+(custom-set-variables '(yas-trigger-key "TAB"))
 
+(define-key yas-minor-mode-map (kbd "C-'") 'yas-expand)
 ;; http://toot-fafsuhiro.hatenablog.com/entry/2013/12/06/133938
-;; (define-key yas-minor-mode-map (kbd "C-x y i") 'yas-insert-snippet)
-;; (define-key yas-minor-mode-map (kbd "C-x y n") 'yas-new-snippet)
-;; (define-key yas-minor-mode-map (kbd "C-x y v") 'yas-visit-snippet-file)
+(define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
+(define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
+(define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
+
+(custom-set-variables '(yas-new-snippet-default "\
+# -*- mode: snippet -*-
+# name: $1
+# key: ${2:${1:$(yas--key-from-desc yas-text)}}
+# expand-env: ((yas/indent-line 'fixed) (yas/wrap-around-region 'nil))
+# --
+$0"))

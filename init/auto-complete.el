b1e699b15a5921fdbbca41354eb10ef58a37701c
Naoki Wakisaka <naoki.wakisaka@ams.eng.osaka-u.ac.jp>
2018-04-06 09:53:03 +0900

    Fix auto-complete

--- init/auto-complete.el
+++ init/auto-complete.el
@@ -1,10 +1,23 @@
 ;; auto-complete
 (el-get-bundle auto-complete)
 
-(global-auto-complete-mode t)
-(setq ac-auto-start 2)
+;; (global-auto-complete-mode t)
+;; (setq ac-auto-start 2)
 
-(add-to-list 'ac-modes 'ess-mode)
-(add-to-list 'ac-modes 'inferior-ess-mode)
+(custom-set-variables
+ `(ac-dictionary-directories ,(concat user-emacs-directory "ac-dict"))
+ '(ac-use-fuzzy t)
+ '(ac-auto-start 2)
+ '(ac-use-menu-map t)
+ '(ac-quick-help-delay 1.0))
+(ac-config-default)
+
 (add-to-list 'ac-modes 'yatex-mode)
-(add-to-list 'ac-modes 'debugger-mode)
+;; (add-to-list 'ac-modes 'ess-mode)
+;; (add-to-list 'ac-modes 'inferior-ess-mode)
+;; (add-to-list 'ac-modes 'debugger-mode)
+
+;; settings below requires yasnippet
+(defun ac-yatex-mode-setup ()
+  (setq ac-sources (append '(ac-source-yasnippet) ac-sources)))
+(add-hook 'yatex-mode-hook 'ac-yatex-mode-setup)
